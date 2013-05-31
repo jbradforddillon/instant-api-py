25403deed80881c575624fd18992a31fe03134fa
Brad Dillon <jbradforddillon@gmail.com>
2013-05-31 00:40:09 -0400

    Initial code, examples, and readme

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-instant-api-py
-==============
+# What is this?
 
-A python script that creates a simple server from a given JSON file.
+This is essentially a python implementation of [waldoj's instant-api](https://github.com/waldoj/instant-api), but with a few handy bonus features. It creates a basic RESTful web service using a JSON file as source. 
+
+# Usage
+
+Unlike it's PHP counterpart, this script doesn't require a running web server with PHP, it uses python's own HTTPServer. Just run it from the command line, like so:
+
+`$ ./instant-api.py`
+
+To specify the port and/or file to serve, just use the `--port` and `--file` attributes, respectively:
+
+`$ ./instant-api.py --port 8080 --file source.json`
+
+One other minor difference/improvement: this script uses the `friendlier/path/format` rather than `?query=strings`, and can go several levels deep in the JSON, with support for arrays. 
+
+# Example
+
+Given this JSON:
+
+```json
+{
+	"colors" : [
+		{
+			"name" : "Red",
+			"hex" : "#FF0000"
+		},
+		{
+			"name" : "Green",
+			"hex" : "#00FF00"
+		},
+		{
+			"name" : "Blue",
+			"hex" : "#0000FF"
+		}
+	]
+}
+``` 
+
+You could use this:
+
+`http://localhost:8080/colors/1`
+
+To retrieve this:
+
+```json
+{
+	"name" : "Green",
+	"hex" : "#00FF00"
+}
+```
---
+++ instant-api.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import string
+import json
+import sys
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser(description='Process some integers.')
+parser.add_argument("-f", "--file", dest="filename",
+                    help="JSON file to serve", metavar="FILE")
+parser.add_argument("-p", "--port", dest="port",
+                    help="Port to listen on", metavar="PORT")
+
+args = parser.parse_args().__dict__
+
+file_arg = args['filename'] or "source.json"
+port_arg = args['port'] or "8080"
+
+try:
+    content = json.loads(open(file_arg).read())
+except IOError:
+    print "Couldn't find file '%s'" % file_arg
+    sys.exit()
+
+port = 8080 if port_arg is None else int(port_arg)
+
+
+class InstantAPIServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        path = self.path[1:]
+        components = string.split(path, '/')
+
+        node = content
+        for component in components:
+            if len(component) == 0 or component == "favicon.ico":
+                continue
+
+            if type(node) == dict:
+                node = node[component]
+
+            elif type(node) == list:
+                node = node[int(component)]
+
+        self.wfile.write(json.dumps(node))
+
+        return
+
+
+try:
+    server = HTTPServer(('', port), InstantAPIServer)
+    print 'Starting instant API server on port %s...' % (port)
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print 'Stopping instant API server...'
+    server.socket.close()
